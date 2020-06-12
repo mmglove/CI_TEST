70670e083521d5c82e4e4f5c5000405ed16f1e5e
mmglove <gmm_email@163.com>
2020-06-12 14:50:55 +0800

    test=develop

---
+++ temp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2019 Baidu.com, Inc. All Rights Reserved
+This module 
+
+Authors: guomengmeng01(guomengmeng01@baidu.com)
+Date: 2020/6/12 14:45
+"""
+import paddle
+import paddleslim
+print("paddle_version:", paddle.__version__)
+print("paddleslim_version:", paddleslim.__file__)
